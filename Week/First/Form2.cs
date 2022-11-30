c4e0166fed6427679e4edcd65b58f01284504617
Twigh4t <overview480@gmail.com>
2022-11-30 13:07:15 +0500

    Создал форму меню с 2 роботающими кнопками(С 1 и со 2 заданием) Второе задание под вопросом подключение, объевление и иницилизацию сделал но не пойму в какую именно форму это закидывать и нужно ли что-то в форму. Подключение выводиться пока забью

---
+++ Week/First/Form2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace First
+{
+    public partial class Form2 : Form
+    {
+        public class Connect
+        {
+            public static MySqlConnection GetConnection(MySqlConnection mySql)
+            {
+                try
+                {
+                    MessageBox.Show("Подключение");
+                    return mySql;
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка" + ex.Message);
+                    return null;
+                }
+
+            }
+        }
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            string connStr = "server=chuc.caseum.ru;port=33333;username=uchebka;password=uchebka;database=uchebka";
+            MySqlConnection conn = new MySqlConnection(connStr);
+
+            try
+            {
+                Connect.GetConnection(conn);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка" + ex.Message);
+            }
+        }
+    }
+}
